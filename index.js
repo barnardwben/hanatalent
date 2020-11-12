0f4a39522d686646e23028a74ce4033f1029ec02
Benjamin Barnard <barnardwben@yahoo.com>
2020-11-12 18:53:53 +0100

    Fixed Bugs

--- index.js
+++ index.js
@@ -38,45 +38,47 @@ exitBubble.addEventListener('click', () => {
 window.addEventListener("scroll", () => {
   // NAVBAR SCROLL EVENT
   window.scrollY > 35 ? navbar.classList.add('darken') : navbar.classList.remove('darken');
+  if (window.location.href === 'https://hanatestsite.netlify.app/' || window.location.href === 'http://127.0.0.1:5500/index.html') {
+    // GRID SET ONE ANIMATION
+    if (window.scrollY > 500) {
+      firstImage.classList.add('slider-from-left');
+      firstImage.classList.remove('opacity');
+      firstText.classList.add('slider-from-right');
+      firstText.classList.remove('opacity');
+    } else if (window.scrollY < 20) {
+      firstImage.classList.remove('slider-from-left');
+      firstImage.classList.add('opacity');
+      firstText.classList.remove('slider-from-right');
+      firstText.classList.add('opacity');
+    }
 
-  // GRID SET ONE ANIMATION
-  if (window.scrollY > 500) {
-    firstImage.classList.add('slider-from-left');
-    firstImage.classList.remove('opacity');
-    firstText.classList.add('slider-from-right');
-    firstText.classList.remove('opacity');
-  } else if (window.scrollY < 20) {
-    firstImage.classList.remove('slider-from-left');
-    firstImage.classList.add('opacity');
-    firstText.classList.remove('slider-from-right');
-    firstText.classList.add('opacity');
+    // GRID SET TWO ANIMATION
+    if (window.scrollY > 1000) {
+      secondImage.classList.add('slider-from-right');
+      secondImage.classList.remove('opacity');
+      secondText.classList.add('slider-from-left');
+      secondText.classList.remove('opacity');
+    } else if (window.scrollY < 20) {
+      secondImage.classList.remove('slider-from-right');
+      secondImage.classList.add('opacity');
+      secondText.classList.remove('slider-from-left');
+      secondText.classList.add('opacity');
+    }
+
+    // GRID SET THREE ANIMATION
+    if (window.scrollY > 1700) {
+      thirdImage.classList.add('slider-from-left');
+      thirdImage.classList.remove('opacity');
+      thirdText.classList.add('slider-from-right');
+      thirdText.classList.remove('opacity');
+    } else if (window.scrollY < 20) {
+      thirdImage.classList.remove('slider-from-left');
+      thirdImage.classList.add('opacity');
+      thirdText.classList.remove('slider-from-right');
+      thirdText.classList.add('opacity');
+    }
   }
 
-  // GRID SET TWO ANIMATION
-  if (window.scrollY > 1000) {
-    secondImage.classList.add('slider-from-right');
-    secondImage.classList.remove('opacity');
-    secondText.classList.add('slider-from-left');
-    secondText.classList.remove('opacity');
-  } else if (window.scrollY < 20) {
-    secondImage.classList.remove('slider-from-right');
-    secondImage.classList.add('opacity');
-    secondText.classList.remove('slider-from-left');
-    secondText.classList.add('opacity');
-  }
-
-  // GRID SET THREE ANIMATION
-  if (window.scrollY > 1700) {
-    thirdImage.classList.add('slider-from-left');
-    thirdImage.classList.remove('opacity');
-    thirdText.classList.add('slider-from-right');
-    thirdText.classList.remove('opacity');
-  } else if (window.scrollY < 20) {
-    thirdImage.classList.remove('slider-from-left');
-    thirdImage.classList.add('opacity');
-    thirdText.classList.remove('slider-from-right');
-    thirdText.classList.add('opacity');
-  }
 
 
 })
@@ -90,59 +92,60 @@ const choiceFour = document.querySelector('.gi-four');
 const leftArr = document.querySelector('.left-arr')
 const rightArr = document.querySelector('.right-arr')
 
-
-choiceOne.addEventListener('click', () => {
-  num = 0;
-  updateDisplay('finacc');
-});
-
-choiceTwo.addEventListener('click', () => {
-  num = 1;
-  updateDisplay('itser');
-});
-
-choiceThree.addEventListener('click', () => {
-  num = 2;
-  updateDisplay('indus');
-});
-choiceFour.addEventListener('click', () => {
-  num = 3;
-  updateDisplay('offad');
-});
-
-
-// FUNCTION FOR SWITCHING BETWEEN HTML CONTENT
-function updateDisplay(serviceType) {
-  let i, servcontent;
-  servcontent = document.getElementsByClassName("servicescontent");
-  for (i = 0; i < servcontent.length; i++) {
-    servcontent[i].style.display = "none";
-  }
-  document.getElementById(serviceType).style.display = "block";
-}
-
-// ON PAGE LOAD SHOW FINANCE 6 ACCOUNTING SERVICES HTML CONTENT
-updateDisplay('finacc');
-
-let serviceArr = ['finacc', 'itser', 'indus', 'offad'];
-
-let num = 0;
-rightArr.addEventListener('click', () => {
-  if (num >= 3) {
-    num = 3;
-
-  } else {
-    num++;
-    updateDisplay(serviceArr[num])
-  }
-})
-
-leftArr.addEventListener('click', () => {
-  if (num <= 0) {
+if (window.location.href === 'http://127.0.0.1:5500/ourfocus.html' || window.location.href === 'https://hanatestsite.netlify.app/ourfocus.html') {
+  choiceOne.addEventListener('click', () => {
     num = 0;
+    updateDisplay('finacc');
+  });
 
-  } else {
-    num--;
-    updateDisplay(serviceArr[num])
+  choiceTwo.addEventListener('click', () => {
+    num = 1;
+    updateDisplay('itser');
+  });
+
+  choiceThree.addEventListener('click', () => {
+    num = 2;
+    updateDisplay('indus');
+  });
+  choiceFour.addEventListener('click', () => {
+    num = 3;
+    updateDisplay('offad');
+  });
+
+
+  // FUNCTION FOR SWITCHING BETWEEN HTML CONTENT
+  function updateDisplay(serviceType) {
+    let i, servcontent;
+    servcontent = document.getElementsByClassName("servicescontent");
+    for (i = 0; i < servcontent.length; i++) {
+      servcontent[i].style.display = "none";
+    }
+    document.getElementById(serviceType).style.display = "block";
   }
-})
+
+  // ON PAGE LOAD SHOW FINANCE 6 ACCOUNTING SERVICES HTML CONTENT
+  updateDisplay('finacc');
+
+  let serviceArr = ['finacc', 'itser', 'indus', 'offad'];
+
+  let num = 0;
+  rightArr.addEventListener('click', () => {
+    if (num >= 3) {
+      num = 3;
+
+    } else {
+      num++;
+      updateDisplay(serviceArr[num])
+    }
+  })
+
+  leftArr.addEventListener('click', () => {
+    if (num <= 0) {
+      num = 0;
+
+    } else {
+      num--;
+      updateDisplay(serviceArr[num])
+    }
+  })
+}
